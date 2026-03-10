64045c20f625b6676904abe816ca066896a51d70
mayconmddias <mayconmddias@gmail.com>
2026-03-10 16:45:55 -0300

    Fix: migrated Tailwind to local for full offline support

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 console.log("React: Checking root element...", rootElement);
